f83af87a52f5e2a34b193d66643e2f9c155947dc
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-03-17 07:26:06 +0200

    -Initialize new react app using vite.
    -Remove unnecessary code and delete unneeded assets.
    -Create the first component which will contain the form which allow end user to add todo item.

---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}

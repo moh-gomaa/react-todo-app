ac7a50faeb8f8d425ae409f5a4dff98daefb7d9d
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-03-18 15:14:32 +0200

    -Add form submission to Todo component which allow user to add todo item. -Implement TodoItem component which displays the added todos.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todosLst, setTodosLst] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodosLst([...todosLst, todo]);
+    setTodo("");
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input
           type="text"
           value={todo}
@@ -13,6 +22,10 @@ export default function Todo() {
         />
         <button type="submit">Add</button>
       </form>
+
+      {todosLst.map((item) => (
+        <TodoItem key={item} item={item} />
+      ))}
     </div>
   );
 }

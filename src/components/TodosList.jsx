22722d734cabb3810533aabc698aa5302cb4f2b6
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2024-03-21 09:55:15 +0200

    -Sort todos according to isDone property.

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -2,9 +2,10 @@ import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
 export default function TodosList({ todosLst, setTodosLst }) {
+  const sortedTodos = todosLst.slice().sort((a, b) => a.isDone - b.isDone);
   return (
     <div className={styles.list}>
-      {todosLst.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem
           key={item.name}
           item={item}
